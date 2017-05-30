e6836e609d613cd694d71b27d87813f42e2fc89b
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 14:07:44 +0700

    Save session for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/home', to: 'commons#home'
   get '/help', to: 'commons#help'
   get '/about', to: 'commons#about'
   get '/contact', to: 'commons#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   root 'commons#home'
 
