9c000792e79b8025dee059f500e2cee09a4795ff
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 11:08:04 +0700

    Add index for email field of users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530032922) do
+ActiveRecord::Schema.define(version: 20170530040355) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
