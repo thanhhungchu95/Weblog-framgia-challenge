ede5017fb2bc0fc633d92dbae601e3f6b2dd9854
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 12:51:08 +0700

    Add show information page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def avatar_for(user)
+    avatar_url = 'male.png'
+    avatar_url = 'female.png' unless user.sex
+
+    image_tag(avatar_url, alt: user.name, class: 'avatar')
+  end
 end
