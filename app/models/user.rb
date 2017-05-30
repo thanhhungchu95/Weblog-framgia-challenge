0f08de9fcbb26e03c02a2c79f9bf200579f269e7
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 15:33:55 +0700

    Add table entries to store micropost of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :entries, dependent: :destroy
+
   before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: 50 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530043536) do
+ActiveRecord::Schema.define(version: 20170530080407) do
+
+  create_table "entries", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
