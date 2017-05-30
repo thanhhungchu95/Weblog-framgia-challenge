bc5e39747a93128814a88afdaa60d92680bb54aa
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 11:12:53 +0700

    Change field birthday of users from datetime to date

---
+++ db/migrate/20170530040856_change_data_type_for_users_birthday.rb
@@ -0,0 +1,5 @@
+class ChangeDataTypeForUsersBirthday < ActiveRecord::Migration[5.0]
+  def change
+    change_column :users, :birthday, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530040355) do
+ActiveRecord::Schema.define(version: 20170530040856) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "birthday"
+    t.date     "birthday"
     t.boolean  "sex"
     t.string   "address"
     t.datetime "created_at", null: false
