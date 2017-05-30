b93403a03343956d1f201cdb9b6f9e5b41528073
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 11:34:54 +0700

    Downcase email before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
