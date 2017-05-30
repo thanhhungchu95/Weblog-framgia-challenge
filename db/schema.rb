2afbb2ee1467a49985568dfd42889e9be4484e2e
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 10:54:53 +0700

    Add model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170530032922) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "birthday"
+    t.boolean  "sex"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
