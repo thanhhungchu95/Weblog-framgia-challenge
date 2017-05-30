e122aca39ab65cab7ef6a1586f1c80f8dd7e40fe
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 09:23:14 +0700

    Setup title layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def weblog_full_title(page_title = '')
+    base_title = 'Weblog - A blogger application'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/home', to: 'commons#home'
+  get '/help', to: 'commons#help'
+  get '/about', to: 'commons#about'
+
+  root 'commons#home'
 end
