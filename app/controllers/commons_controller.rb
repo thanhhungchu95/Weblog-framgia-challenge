8a4ae22742561753a6b53ac865ecb5ada122342e
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-05-30 10:00:54 +0700

    Add contact page

--- app/controllers/commons_controller.rb
+++ app/controllers/commons_controller.rb
@@ -7,4 +7,7 @@ class CommonsController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/home', to: 'commons#home'
   get '/help', to: 'commons#help'
   get '/about', to: 'commons#about'
+  get '/contact', to: 'commons#contact'
 
   root 'commons#home'
 end
